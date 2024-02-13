ca99019efa7bde1830251a0410ba514aeb5b1ada
Goto Yukio <yukio.goto@enigmo.co.jp>
2024-02-13 16:12:24 +0900

    👍 modify logic to update create time and exif info by basename

--- chtime.rb
+++ chtime.rb
@@ -3,9 +3,12 @@
 require 'bundler/setup'
 require 'time'
 require 'mini_exiftool'
+require_relative 'lib/file_tools'
 
 def update_utime(dir)
   # puts "-----------"
+  old_date = ''
+
   list = Dir.glob(File.join(dir, '*')).sort
   i = 0
   list.each do |f|
@@ -14,33 +17,33 @@ def update_utime(dir)
       update_utime(f)
       next
     end
-    
-    dirname = File.dirname(f)
-    # puts dirname
-    parent_name = File.basename(dirname)
-    # puts parent_name
-    next unless parent_name =~
+
+    bname = File.basename(f)
+    next unless bname =~
                 /.*([0-9]{4})\.*([0-9]{2})\.*([0-9]{2})/
+
+    if old_date == "#{Regexp.last_match[1]}#{Regexp.last_match[2]}#{Regexp.last_match[3]}"
+      i += 1
+    else
+      old_date = "#{Regexp.last_match[1]}#{Regexp.last_match[2]}#{Regexp.last_match[3]}"
+      i = 0
+    end
 
     file_time = Time.strptime(Regexp.last_match[1] +
                               Regexp.last_match[2] +
                               Regexp.last_match[3],
                               '%Y%m%d') + i
 
-    exif_data = MiniExiftool.new(f)
-    if exif_data && exif_data['CreateDate']    
-      file_time = exif_data['CreateDate']
-    end
     puts file_time
-    i += 1
-    File.utime(file_time,
-               file_time,
-               f)
-    cmd = "setfile -d \"#{file_time.strftime('%m/%d/%Y %H:%M:%S')}\" \"#{f}\""
-    puts cmd
-    `#{cmd}`
+
+    change_exif_date_time_original(f, file_time)
+    change_ctime(f, file_time)
   end
 end
 
-update_utime(ARGV[0])
+if ARGV.size != 1
+  puts 'usage: bundle exec ruby chtime TARGET_DIR'
+  exit 1
+end
 
+update_utime(ARGV[0])
